8bceeb393a036c91208f3184ed7a636f376e5a1f
surajyaligar2004 <surajyaligar18@gmail.com>
2024-10-13 16:22:53 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-green-500">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
